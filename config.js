b0b4e63fde58e94ec439e472837850d2b783ec77
na57 <na57@qq.com>
2015-12-24 16:02:19 +0800

    添加sidebar

--- config.js
+++ config.js
@@ -5,8 +5,20 @@
  */
 
 export default {
-  title: 'React Static Boilerplate',
-  description: 'Static website generator for React.js web applications.',
+  title: '家琨教育基金会',
+  description: '家琨教育基金会',
   googleAnalyticsId: 'UA-XXXXX-X',
+  sidebar: [
+  {
+  	title: '首页',
+  	icon: 'fa-home',
+  	href: '/'
+  },
+  {
+  	title: '电子阅览室',
+  	icon: '',
+  	href: '/reading-room'
+  }
+  ]
 };
 
