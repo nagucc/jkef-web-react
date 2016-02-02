69915e9e5112c8a976232774de0cbbb5194d1836
na57 <na57@qq.com>
2016-02-02 13:24:06 +0800

    删除多余代码

--- src/api/wx-ent/signup.js
+++ src/api/wx-ent/signup.js
@@ -2,7 +2,7 @@
 使用基于微信企业号的用户认证
 */
 
-import { host, wxentSignupConfig as wxcfg, redisConfig as redis } from '../../config';
+import { wxentSignupConfig as wxcfg, redisConfig as redis } from '../../config';
 import { Router } from 'express';
 import API from 'wxent-api-redis';
 import uuid from 'node-uuid';
