eecf842b73737bb6966051128ef80dd575c5a9bd
na57 <na57@qq.com>
2016-02-02 13:25:32 +0800

    增加一些功能

--- src/api/jkef/acceptors.js
+++ src/api/jkef/acceptors.js
@@ -1,4 +1,8 @@
+import { wxentAdminConfig as wxcfg, redisConfig as redis } from '../../config';
 import { Router } from 'express';
+import API from 'wxent-api-redis';
+import uuid from 'node-uuid';
+import errmsg from 'wx-errmsg';
 import AcceptorManager from './model.js';
 
 const router = new Router();
@@ -8,7 +12,7 @@ var search = async (req, res, next) => {
 	var text = req.params.text || '';
 	var page = req.query.page || 0;
 	var size = req.query.size || 20;
-	AM.search(text, {_id:1, name: 1, isMale: 1, phone: 1, idCard:1 }, (err, acceptors) => {
+	AM.search(text, {_id:1, name: 1, isMale: 1, phone: 1, idCard:1}, (err, acceptors) => {
 		if(err) res.send({ret: -1, msg: err});
       	else res.send({ret: 0, data: acceptors});
 	}, page*size, size);
@@ -29,6 +33,15 @@ var getCount = async (req, res, next) => {
 router.get('/count/:text', getCount);
 router.get('/count', getCount);
 
+// 新增受赠人信息
+router.put('/', async (req, res, next) => {
+	console.log(req.body);
+	if(!req.body.name || !req.body.phone){
+		res.send({ret: -1, msg: '姓名和手机号不能为空'});
+	}
+	res.send({ret:0});
+});
+
 
 // 必须放在最后匹配
 router.get('/:id', (req, res, next) => {
@@ -36,6 +49,11 @@ router.get('/:id', (req, res, next) => {
 		if(err) res.send({ret: -1, msg: err});
       	else res.send({ret: 0, data: result});
 	});
+});
+
+router.post('/:id', (req, res, next) => {
+	console.log(req.body);
+	res.send({ret: 0});
 });
 
 export default router;
