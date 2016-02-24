b0f2b4f17f713fe1ab7cb7a4d69e6951b4d78699
na57 <na57@qq.com>
2016-02-24 13:44:40 +0800

    add NavBar container

---
+++ src/containers/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { connect } from 'react-redux'
+import NavBar from '../../components/Ace/NavBar';
+
+const mapStateToProps = (state, ownProps) => {
+	return {
+		enableUserInfo: state.enableUserInfo
+	}
+}
+export default connect(mapStateToProps)(NavBar);
