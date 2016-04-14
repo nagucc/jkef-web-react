78b0245ca505ef85e16d988775ae9a2da4119e82
na57 <na57@qq.com>
2016-04-14 15:12:01 +0800

    设置好HTML

--- src/components/Html/Html.js
+++ src/components/Html/Html.js
@@ -29,39 +29,14 @@ class Html extends Component {
     return (
 <html lang="en">
   <head>
-    <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <meta charSet="utf-8" />
-    <title>固定资产管理</title>
-
-    <meta name="description" content="" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-
-    <link rel="stylesheet" href="/assets/css/bootstrap.css" />
-    <link rel="stylesheet" href="/assets/css/font-awesome.css" />
-
-
-
-    <link rel="stylesheet" href="/assets/css/ace-fonts.css" />
-
-    <link rel="stylesheet" href="/assets/css/ace.css" className="ace-main-stylesheet" id="main-ace-style" />
-
-    <script src="/assets/js/ace-extra.js"></script>
-
+    <meta charSet="UTF-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0" />
+    <link rel="stylesheet" href="	//res.wx.qq.com/open/libs/weui/0.4.1/weui.css"/>
+    <link rel="stylesheet" href="	./public/site.css"/>
+    <title>react-weui</title>
   </head>
   <body className="no-skin">
     <div id="app" dangerouslySetInnerHTML={{ __html: this.props.body }}/>
-
-    <script src='/assets/js/jquery.js'></script>
-    <script src="/assets/js/bootstrap.js"></script>
-
-
-    <script src="/assets/js/ace/ace.js"></script>
-    <script src="/assets/js/ace/ace.sidebar.js"></script>
-    <script src="/assets/js/ace/ace.sidebar-scroll-1.js"></script>
-    <script src="/assets/js/ace/ace.submenu-hover.js"></script>
-    <script src="/assets/js/ace/ace.widget-box.js"></script>
-    <script src="/assets/js/ace/ace.widget-on-reload.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js"></script>
 
     <script src={this.props.entry}></script>
   </body>
