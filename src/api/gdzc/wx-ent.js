0576acff13de5de62e3c19536b8ed4d71b7e4789
na57 <na57@qq.com>
2016-04-18 15:02:41 +0800

    add: 我领用的

--- src/api/gdzc/wx-ent.js
+++ src/api/gdzc/wx-ent.js
@@ -12,13 +12,29 @@ const wxapi = API(wxcfg.corpId, wxcfg.secret, wxcfg.agentId, redis.host, redis.p
 
 
 router.use('/test', async(req, res, next) => {
-  var url = wxapi.getAuthorizeURL(`http://gdzc.nagu.cc/api/gdzc-wx-ent/wxback`, '', 'snsapi_base');
+  var url = wxapi.getAuthorizeURL(`http://gdzc.nagu.cc/api/gdzc-wx-ent/lyr`, 'state', 'snsapi_base');
   console.log(url);
   res.send(url);
 });
 
-router.get('/wxback', (req, res, next) => {
-  res.send('back：', req.query.code);
-})
+
+router.get('/lyr', (req, res, next) => {
+  // 使用code获取userId
+  wxapi.getUserIdByCode(req.query.code, (err, result) => {
+    if(err) {
+      res.send({ret: -1, msg: err});
+    } else {
+      // 用户验证正确，设置用户状态为登录，获取用户姓名，并转向/items?lyr=xx页面
+      wxapi.getUser(result.UserId, (err, user) => {
+        res.redirect(`http://gdzc.nagu.cc/items?lyr=${user.name}`);
+      });
+    }
+  });
+});
+
+
+// router.use('/', wxent(wxcfg).text((msg, req, res, next) => {
+//
+// }).middleware());
 
 export default router;
